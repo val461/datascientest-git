485ac34180b5e87075d391ca87e435478ce4e110
Valentin MARIE <6820182+val461@users.noreply.github.com>
2025-01-18 18:15:37 +0000

    -

---
+++ MongoDB/exam_MongoDB_MARIE/a.py
@@ -0,0 +1,104 @@
+# (a)
+
+from pymongo import MongoClient
+
+from pprint import pprint
+
+client = MongoClient(
+    host="127.0.0.1",
+    port = 27017,
+    username = "datascientest",
+    password = "dst123"
+)
+
+def p(l):
+    pprint(list(l))
+
+# (b)
+
+print(client.list_database_names())
+
+# (c)
+
+sample = client["sample"]
+print(sample.list_collection_names())
+
+# (d)
+
+col = sample.books
+pprint(col.find_one())
+
+# (e)
+
+print(col.count_documents({}))
+
+# (a)
+
+print(col.count_documents({'pageCount': {'$gt': 400}}))
+print(col.count_documents({'$and':[
+    {'pageCount': {'$gt': 400}},
+    {'status': 'PUBLISH'}
+    ]}))
+
+# (b)
+
+import re
+regex = re.compile("Android")
+print(col.count_documents(({'$or':[
+    {'shortDescription': regex},
+    {'longDescription': regex}
+    ]})))
+
+# (c)
+
+p(col.aggregate([
+    {'$group':
+        {'_id': '$categories'
+    }}
+]))
+
+
+# separate results
+p(col.aggregate([
+    {'$group':
+        {'_id': '$categories',
+        'c0': {'$addToSet': {'$arrayElemAt': ['$categories',0]}},
+        'c1': {'$addToSet': {'$arrayElemAt': ['$categories',1]}}
+    }},
+    {'$project': {'_id': 0}}
+]))
+
+p(col.aggregate([
+    {'$group':
+        {'_id': None,
+        'c0': {'$addToSet': {'$arrayElemAt': ['$categories',0]}},
+        'c1': {'$addToSet': {'$arrayElemAt': ['$categories',1]}}
+    }}
+]))
+
+
+# error
+p(col.aggregate([
+    {'$group':
+        {'_id': '$categories',}},
+    {'$project': {'set_0':
+        {'$addToSet':
+            {'$arrayElemAt': ['$_id',0]}
+    }}}
+]))
+
+# error
+p(col.aggregate([
+    {'$group':
+        {'_id': '$categories',}},
+    {'$addToSet': {'set_0':
+        {'$addToSet':
+            {'$arrayElemAt': ['$_id',0]}
+    }}}
+]))
+
+
+
+# ()
+
+
