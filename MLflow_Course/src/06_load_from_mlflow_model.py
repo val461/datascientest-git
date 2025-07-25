43e8db9863278b57efadb6a666dc905c4321e020
val461 <6820182+val461@users.noreply.github.com>
2025-07-25 14:19:40 +0000

    -

--- MLflow_Course/src/06_load_from_mlflow_model.py
+++ MLflow_Course/src/06_load_from_mlflow_model.py
@@ -3,15 +3,13 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 
 # 1. Chargement des données
-# TODO: Remplacer avec le chemin vers votre jeu de données
 print("Chargement des données...")
-data = pd.read_csv(None)
+data = pd.read_csv('data/fake_data.csv')
 X = data.drop(columns=["date", "demand"])
 X = X.astype('float')
 
 # 2. Définir le chemin vers le modèle MLflow
-# TODO: Remplacer avec le chemin vers votre dossier "rf_apples" créé précédemment
-model_path = ''  # Par exemple : '/home/ubuntu/MLflow/mlruns/EXPERIMENT_ID/RUN_ID/artifacts/rf_apples'
+model_path = '/home/ubuntu/MLflow_Course/mlruns/534228407498600468/64abd802e94b44f7bdfe2a267462430b/artifacts/rf_apples'  # Par exemple : '/home/ubuntu/MLflow/mlruns/EXPERIMENT_ID/RUN_ID/artifacts/rf_apples'
 
 # 3. Charger le modèle
 print("Chargement du modèle...")
@@ -22,9 +20,14 @@ print("Calcul des prédictions...")
 predictions = model.predict(X)
 
 # 5. Calculer et afficher la moyenne des prédictions
-# TODO: Calculer la moyenne des prédictions
-mean_prediction = 0  # Utiliser la fonction appropriée de numpy ou pandas
+mean_prediction = predictions.mean()  # Utiliser la fonction appropriée de numpy ou pandas
 
 print(f"\nRésultats :")
 print(f"Nombre de prédictions : {len(predictions)}")
 print(f"Moyenne des prédictions : {mean_prediction:.2f}")
+
+# mlflow models serve \
+#         --model-uri '/home/ubuntu/MLflow_Course/mlruns/534228407498600468/64abd802e94b44f7bdfe2a267462430b/artifacts/rf_apples' \
+#         --port 5002 \
+#         --host 0.0.0.0 \
+#         --env-manager local
