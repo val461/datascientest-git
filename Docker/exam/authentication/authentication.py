bd2ccebb9f3eb9b402f2b2db216c994a42d389af
val461 <6820182+val461@users.noreply.github.com>
2025-12-17 12:13:26 +0000

    cancel undue copy paste to authentication.py

--- Docker/exam/authentication/authentication.py
+++ Docker/exam/authentication/authentication.py
@@ -3,46 +3,42 @@ import requests
 
 api_address = 'my_api_from_compose'
 api_port = 8000
-pages = ['v1/sentiment','v2/sentiment']
-tests = [('alice','wonderland',[200,200]),('bob','builder',[200,403])]
+page = 'permissions'
+tests = [('alice','wonderland',200),('bob','builder',200), ('clementine','mandarine',403)]
 
-for user, password, expected_results in tests:
-    for k, page in enumerate(pages):
-        params={
-            'username': user,
-            'password': password,
-            'sentence': 'not a negative sentence'
-        }
-        # requête
-        r = requests.get(
-            url=f'http://{api_address}:{api_port}/{page}',
-            params=params
-        )
-        # statut de la requête
-        status_code = r.status_code
+for user, password, expected_result in tests:
+    params={
+        'username': user,
+        'password': password,
+    }
+    r = requests.get(
+        url=f'http://{api_address}:{api_port}/{page}',
+        params=params
+    )
+    # statut de la requête
+    status_code = r.status_code
 
-        # affichage des résultats
-        expected_result = expected_results[k]
-        if status_code == expected_result:
-            test_status = 'SUCCESS'
-        else:
-            test_status = 'FAILURE'
+    # affichage des résultats
+    if status_code == expected_result:
+        test_status = 'SUCCESS'
+    else:
+        test_status = 'FAILURE'
 
-        output = f'''
-        ============================
-            Authentication test
-        ============================
+    output = f'''
+    ============================
+        Authentication test
+    ============================
 
-        request done at "/{page}"
-        {params}
+    request done at "/{page}"
+    {params}
 
-        Expected result = {expected_result};
-        actual result = {status_code}
+    Expected result = {expected_result};
+    actual result = {status_code}
 
-        ==>  {test_status}
+    ==>  {test_status}
 
-        '''
-        print(output)
-        if os.environ.get('LOG') == 1:
-            with open('log/api_test.log', 'a') as file:
-                file.write(output)
+    '''
+    print(output)
+    if os.environ.get('LOG') == '1':
+        with open('log/api_test.log', 'a') as file:
+            file.write(output)
